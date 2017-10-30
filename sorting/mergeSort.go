92132dd195331ff6c9c7b8fea740e1094081120a
Callum Styan <callumstyan@gmail.com>
2017-10-29 18:59:28 -0700

    add merge sort

---
+++ sorting/mergeSort.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+// standard solution using indicies
+func merge(left, right []int) []int {
+	leftIndex := 0
+	rightIndex := 0
+	lenLeft := len(left)
+	lenRight := len(right)
+	var merged []int
+	for leftIndex < lenLeft && rightIndex < lenRight {
+		if left[leftIndex] < right[rightIndex] {
+			merged = append(merged, left[leftIndex])
+			leftIndex = leftIndex + 1
+		} else {
+			merged = append(merged, right[rightIndex])
+			rightIndex = rightIndex + 1
+		}
+	}
+	if leftIndex < lenLeft {
+		merged = append(merged, left[leftIndex:]...)
+	}
+	if rightIndex < lenRight {
+		merged = append(merged, right[rightIndex:]...)
+	}
+	return merged
+}
+
+// splits current array in half, determines if halves are ready to be merged
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	// len(arr)/2 rounds down
+	// arr[:num] is exlusive, does not include element at index num
+	// arr[num:] is inclusive, does include element at index num
+	left := arr[:len(arr)/2]
+	right := arr[len(arr)/2:]
+	if len(left) != 1 {
+		left = mergeSort(left)
+	}
+	if len(right) != 1 {
+		right = mergeSort(right)
+	}
+	return merge(left, right)
+}
+
+func main() {
+	array := []int{1, 6, 2, 4, 9, 0, 5, 3, 7, 13, 11}
+	fmt.Println("original array: ", array)
+	fmt.Println("merged: ", mergeSort(array))
+}
